290d63e33e14d65538d9e5880393431845bfc75a
federicogiorgino <federicogiorgino@protonmail.com>
2020-02-21 13:06:09 +0100

    Endpoint + Render /year page

---
+++ public/javascripts/get_axios.js
@@ -0,0 +1,39 @@
+// const axios = require("axios");
+
+// axios
+//   .get("http://localhost:3000/timeline")
+//   .then(resp => {
+//     data = resp.data;
+//     data.forEach(element => {
+//       console.log(`${element.year}`);
+//     });
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// axios
+//   .get("http://localhost:3000/timeline/year?ID")
+//   .then(resp => {
+//     data = resp.data;
+//     data.forEach(element => {
+//       console.log(`${element.title}, ${element.details}`);
+//     });
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// axios
+//   .get("http://localhost:3000/timeline/year/event")
+//   .then(resp => {
+//     data = resp.data;
+//     data.forEach(element => {});
+//     element => {
+//       console.log(`${element.title}, ${element.article}`);
+//     };
+//   })
+
+//   .catch(error => {
+//     console.log(error);
+//   });
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+//require and npm i axios
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET /timeline */
+router.get("/year", function(req, res, next) {
+  axios
+    .get("https://hack-usa-jasondata.herokuapp.com/year")
+    .then(resp => {
+      data = resp.data;
+      res.render("index", { data });
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+/* GET /timeline/year */
+
+router.get("/year/:year", function(req, res, next) {
+  const { year } = req.params;
+  axios
+    .get(`https://hack-usa-jasondata.herokuapp.com/year?year=${year}`)
+    .then(resp => {
+      data = resp.data;
+      res.render();
+    })
+    .catch(error => {
+      console.log(error);
+    });
+});
+
+/* GET /timeline/year/event */
+
+router.get("/year/:year/:event", (req, res) => {
+  axios
+    .get(`https://hack-usa-jasondata.herokuapp.com/year?year=${year}&event=${event}`)
+    .then(resp => {
+      data = resp.data;
+      data.forEach(element => {});
+      element => {
+        console.log(data);
+      };
+    })
+
+    .catch(error => {
+      console.log(error);
+    });
 });
 
 module.exports = router;
